bf58b00a8f76f0e5f949cdfcbe5abd2e26dfc733
Samueljustiniani <samuel.justiniani.lancho@vallegrande.edu.pe>
2024-09-24 12:07:45 -0500

    feat:add header.js

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-// Aqui va el codigo del header.js
+// Aqui va el codigo del header.js el que le esto es alto kbro homosexualizado
 const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 60)
